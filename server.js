b24740cc7b2380703d548f660ee4d2643ffae59a
dumpsterRat92 <you@example.com>
2024-05-02 19:03:34 -0700

    added route for homepage and notes page added comments

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
+// Importing required modules
 const express = require('express');
 const routes = require('./routes');
 
+// Setting up the port
 const PORT = process.env.PORT || 3001;
 
+// Creating an instance of Express
 const app = express();
 
+// Serving static files from the 'public' directory
 app.use(express.static('public'));
+
+// Parsing incoming JSON data
 app.use(express.json());
 
+// Route for the homepage
 app.get('/', (req, res) => {
-    res.sendFile(`index.html`);
+    res.sendFile(`index.html`); // Sending the index.html file
 });
 
+// Route for the notes page
 app.get('/notes', (req, res) => {
-    res.sendFile(`${__dirname}/public/notes.html`);
+    res.sendFile(`${__dirname}/public/notes.html`); // Sending the notes.html file
 });
 
+// Using routes defined in separate files
 app.use(routes);
 
+// Starting the server and listening on the specified port
 app.listen(PORT, () =>
     console.log(`listening at http://localhost:${PORT}`)
 );
